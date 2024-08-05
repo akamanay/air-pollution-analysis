118f4a2abf7dff517202f175dd54810bd2cc08cd
Harenabs21 <hei.harena.6@gmail.com>
2024-08-05 17:17:16 +0300

    chore: include the time in filename

--- dags/tasks/load.py
+++ dags/tasks/load.py
@@ -9,15 +9,15 @@ def load_to_csv(dataframe, output_path):
     :param dataframe: pd.DataFrame, the DataFrame to be saved
     :param output_path: str, the base path for the output file
     """
-    # Get the current date in YYYY-MM-DD format
-    current_date = datetime.now().strftime("%Y-%m-%d")
+    # Get the current datetime in YYYY-MM-DD-HH-MM format
+    current_date_time = datetime.now().strftime("%Y-%m-%d-%H-%M")
 
     # Construct the filename with the current date
-    filename = f"weather-{current_date}.csv"
+    filename = f'weather-{current_date_time}.csv'
 
     # Full path for the output file
     full_output_path = os.path.join(output_path, filename)
 
     # Save the DataFrame to CSV
     dataframe.to_csv(full_output_path, index=False)
-    print(f"Data successfully loaded to {full_output_path}")
+    print(f'Data successfully loaded to {full_output_path}')
