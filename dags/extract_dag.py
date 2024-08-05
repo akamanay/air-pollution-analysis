0e89aadbc711f54561e8dece19276797e6946df5
Harenabs21 <hei.harena.6@gmail.com>
2024-08-05 16:47:49 +0300

    feat: define the tasks to run in dags

--- dags/extract_dag.py
+++ dags/extract_dag.py
@@ -1,12 +1,18 @@
 from airflow import DAG
-from airflow.operators.python_operator import PythonOperator
+from airflow.operators.python import PythonOperator
 from airflow.utils.dates import days_ago
 import os
-import functools
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
 
 # Importing the task functions
-from extract.extract_data import extract_data
+from tasks.extract_data import extract_data
+from tasks.transform import transform
+from tasks.load import load_to_csv 
 
+# Default arguments for the DAG
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
@@ -15,35 +21,64 @@ default_args = {
     'retries': 1,
 }
 
-dag = DAG(
-    'first_extract_dag',
+# Define the DAG
+with DAG(
+    'etl_pipeline',
     default_args=default_args,
-    description='First extract DAG',
+    description='ETL pipeline for extracting, transforming, and loading data',
     schedule_interval='@daily',
     start_date=days_ago(1),
     catchup=False,
-)
+) as dag:
 
-DATA_PATH = os.getenv('AIRFLOW_HOME') + '/data'
-# Define the tasks
-extract_demo = PythonOperator(
-    task_id='extract_1',
-    python_callable=functools.partial(extract_data, DATA_PATH + '/Demographic_Data.csv', 'csv'),
-    dag=dag,
-)
+    # Define data paths
+    DATA_PATH = os.path.join(os.getenv('AIRFLOW_HOME'), 'data')
+    DEMO_PATH = os.path.join(DATA_PATH, 'Demographic_Data.csv')
+    GEO_PATH = os.path.join(DATA_PATH, 'Geographic_Data.csv')
 
-extract_geo = PythonOperator(
-    task_id='extract_2',
-    python_callable=functools.partial(extract_data, DATA_PATH + '/Geographic_Data.csv', 'csv'),
-    dag=dag,
-)
+    def extract_demo_task(ti):
+        """Task to extract demographic data."""
+        demographic_data = extract_data(DEMO_PATH, 'csv')
+        ti.xcom_push(key='demographic_data', value=demographic_data)
 
-extract_simple_api = PythonOperator(
-    task_id='extract_3',
-    python_callable=functools.partial(extract_data, 'https://jsonplaceholder.typicode.com/posts', 'api'),
-    dag=dag,
-)
+    def extract_geo_task(ti):
+        """Task to extract geographic data."""
+        geographic_data = extract_data(GEO_PATH, 'csv')
+        ti.xcom_push(key='geographic_data', value=geographic_data)
 
-# Set the task dependencies
-#[extract_demo, extract_geo, extract_simple_api] >> transform >> load
-extract_demo >> extract_geo >> extract_simple_api
+    def transform_task(ti):
+        """Task to transform the extracted data."""
+        demographic_data = ti.xcom_pull(key='demographic_data')
+        geographic_data = ti.xcom_pull(key='geographic_data')
+        transformed_data = transform(demographic_data, geographic_data)
+        ti.xcom_push(key='transformed_data', value=transformed_data)
+
+    def load_task(ti):
+        """Task to load transformed data to a CSV file with a dynamic filename."""
+        transformed_data = ti.xcom_pull(key='transformed_data')
+        output_path = os.getenv('OUTPUT_DATA_PATH')
+        load_to_csv(transformed_data, output_path)
+
+    # Define tasks using PythonOperator
+    extract_demo = PythonOperator(
+        task_id='extract_demo_task',
+        python_callable=extract_demo_task,
+    )
+
+    extract_geo = PythonOperator(
+        task_id='extract_geo_task',
+        python_callable=extract_geo_task,
+    )
+
+    transform_operator = PythonOperator(
+        task_id='transform_task',
+        python_callable=transform_task,
+    )
+
+    load_operator = PythonOperator(
+        task_id='load_task',
+        python_callable=load_task,
+    )
+
+    # Set task dependencies
+    [extract_demo, extract_geo] >> transform_operator >> load_operator
